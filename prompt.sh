ccd8249a5819e0cb2d5e8ea0ed69698aeea0c1d7
Rocka84 <f.dillmeier@gmail.com>
2017-09-21 20:57:36 +0200

    moved all icons to vars, rearranged vars

--- prompt.sh
+++ prompt.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
 # Todo:
-# - rename vars
-# - all icons as vars
 # - refine default colors
-# - README.md
 
 # export PS1='$('"$HOME"'/powerprompt/prompt.sh "$?")'
 
@@ -14,30 +11,53 @@ source "$(dirname "$0")/gitstatus.sh"
 resetColor="\E[0m"
 
 initTheme() {
-	seperator_char=""
+	separator_char=""
+
+
+    command_icon_ok="✔"
+    command_icon_fail="✘"
 
     command_color_bg="18"
     command_color_ok="46"
     command_color_fail="160"
 
+
     pwd_color_bg="25"
     pwd_color_fg="250"
 
+    branch_icon=""
     branch_color_bg="19"
     branch_color_fg="248"
-    branch_color_ahead="252"
-    branch_color_behind="202"
+
     branch_icon_ahead="▲"
+    branch_color_ahead="252"
+
     branch_icon_behind=" "
+    branch_color_behind="202"
+
 
     status_color_bg="26"
     status_color_fg="172"
+
+	status_icon_changed=" "
     status_color_changed="178"
+	
+    status_icon_staged=" "
     status_color_staged="40"
+
+    status_icon_stashed=" "
     status_color_stashed="219"
+
+    status_icon_clean="$command_icon_ok"
+    status_icon_not_clean="$command_icon_fail"
     status_color_clean="46"
+
+    status_icon_untracked=" "
     status_color_untracked="172"
+
+    status_icon_conflicts=" "
     status_color_conflicts="124"
+
 
     shell_color_bg="0"
 }
@@ -56,7 +76,7 @@ setColors() { # 1=newFG, 2=newBG
 nextSegment() {
 	setColors "$1" "$2"
     if [ -n "$last_bg" ]; then
-		add " $(color $last_bg $current_bg)${seperator_char}"
+		add " $(color $last_bg $current_bg)${separator_char}"
 	fi
     add "$(color $current_fg $current_bg) "
 }
@@ -69,20 +89,20 @@ createSegmentGitBranch() {
     [ -z "$git_branch" ] && return
 
     nextSegment "$branch_color_fg" "$branch_color_bg"
-    add " ${git_branch}"
-    [ "$git_behind" -gt 0 ] && add " $(color $branch_color_behind) $git_behind"
-    [ "$git_ahead" -gt 0 ] && add " $(color $branch_color_ahead)▲$git_ahead"
+    add "$branch_icon ${git_branch}"
+    [ "$git_behind" -gt 0 ] && add " $(color $branch_color_behind)${branch_icon_behind}${git_behind}"
+    [ "$git_ahead" -gt 0 ] && add " $(color $branch_color_ahead)${branch_icon_ahead}${git_ahead}"
 }
 
 createSegmentGitStatus() {
     [ -z "$git_branch" ] && return
 
     status=""
-    [ "$git_staged" -gt 0 ] &&    status="${status}$(color $status_color_staged) ${git_staged} "
-    [ "$git_conflicts" -gt 0 ] && status="${status}$(color $status_color_conflicts) ${git_conflicts} "
-    [ "$git_changed" -gt 0 ] &&   status="${status}$(color $status_color_changed) ${git_changed} "
-    [ "$git_untracked" -gt 0 ] && status="${status}$(color $status_color_untracked) ${git_untracked} "
-    [ "$git_stashed" -gt 0 ] &&   status="${status}$(color $status_color_stashed) ${git_stashed} "
+    [ "$git_staged" -gt 0 ] &&    status="${status}$(color $status_color_staged)${status_icon_staged}${git_staged} "
+    [ "$git_conflicts" -gt 0 ] && status="${status}$(color $status_color_conflicts)${status_icon_conflicts}${git_conflicts} "
+	[ "$git_changed" -gt 0 ] &&   status="${status}$(color $status_color_changed)${status_icon_changed}${git_changed} "
+    [ "$git_untracked" -gt 0 ] && status="${status}$(color $status_color_untracked)${status_icon_untracked}${git_untracked} "
+    [ "$git_stashed" -gt 0 ] &&   status="${status}$(color $status_color_stashed)${status_icon_stashed}${git_stashed} "
 
     [ -z "$status" ] && return
 
@@ -93,9 +113,9 @@ createSegmentGitStatus() {
 createSegmentLastCommand() {
     nextSegment "$command_color_ok" "$command_color_bg"
     if [ "$last_command_status" == "0" ]; then
-        add "$(color $current_fg)✔"
+        add "$(color $current_fg)$command_icon_fail"
     else
-        add "$(color $command_color_fail)✘ $last_command_status"
+        add "$(color $command_color_fail)$command_icon_ok $last_command_status"
     fi
 }
 
